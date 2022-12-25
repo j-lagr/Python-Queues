7d94b0901bb17d398d8b261f71191886e40b1ae5
j-lagr <joshualagrata@gmail.com>
2022-12-26 00:27:15 +0800

    testing the iter and len func

--- fifo.py
+++ fifo.py
@@ -1,9 +1,10 @@
 from queues import Queue
-fifo = Queue()
-fifo.enqueue("1st")
-fifo.enqueue("2nd")
-fifo.enqueue("3rd")
 
-print(fifo.dequeue())
-print(fifo.dequeue())
-print(fifo.dequeue())
+fifo = Queue("1st", "2nd", "3rd")
+
+print(len(fifo))
+
+for element in fifo:
+    print(element)
+
+print(len(fifo))
