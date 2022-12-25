ccfca677917c58fdc807a6e671f677c8dfd6203c
j-lagr <joshualagrata@gmail.com>
2022-12-26 00:19:56 +0800

    module is working

--- fifo.py
+++ fifo.py
@@ -4,9 +4,6 @@ fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
-fifo.dequeue()
-'1st'
-fifo.dequeue()
-'2nd'
-fifo.dequeue()
-'3rd'
+print(fifo.dequeue())
+print(fifo.dequeue())
+print(fifo.dequeue())
