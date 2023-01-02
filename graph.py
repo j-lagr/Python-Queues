9c7e590b90936c76691bf3a185a672b0d62d4006
j-lagr <joshualagrata@gmail.com>
2023-01-03 00:55:31 +0800

    changed file name

---
+++ graph.py
@@ -0,0 +1,29 @@
+import networkx as nx
+from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
+def load_graph(filename, node_factory):
+    graph = nx.nx_agraph.read_dot(filename)
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
