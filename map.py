b361ed6639b50adb0c50e9ab91ffbc1fd62c5290
j-lagr <joshualagrata@gmail.com>
2023-01-03 01:00:41 +0800

    breadth-first search using a fifo queue

--- map.py
+++ map.py
@@ -2,3 +2,20 @@ from graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print(nodes["london"])
+print(graph)
+
+for neighbor in graph.neighbors(nodes["london"]):
+    print(neighbor.name)
+
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
+
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
